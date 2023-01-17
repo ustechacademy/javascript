c5a64ac1e6bbd6c4154c02567728295e2e4826c2
ustechacademy <ustechacademytool@gmail.com>
2023-01-17 22:09:43 +0100

    Functions and Objects

--- js/functions.js
+++ js/functions.js
@@ -118,7 +118,7 @@ function faktoriyelHesapla(x) {
   }
 }
 console.log(faktoriyelHesapla(5));
-*/
+
 
 function getFuctorial(y){
     if (y == 0 ){
@@ -159,5 +159,115 @@ function ters(number){
     return parseInt(str2);
 }
 console.log(ters(str));
+*/
+
+// Global Scope
+
+ // Global Variable
+
+var scope = "Global";
 
 
+function access(){
+  let car = "BMW"; // Local Variable
+  console.log(car);
+  console.log(scope);
+}
+access();
+
+//console.log(car);
+
+var fruit = "Orange";
+
+function food(){
+  var fruit = "Apple";
+  let vegetable = "Broccoli";
+  const drinks = "Cola";
+}
+
+console.log(fruit);
+food();
+console.log(fruit);
+
+//console.log(drinks);
+//console.log(vegetable);
+//console.log(fruit);
+
+
+//Objects 
+// key & values
+let person1 = ["Micheal","Mic",35];
+const person = {firstName: "Micheal", lastName:"Mic", age: 35};
+
+console.log(person.lastName);
+console.log(person.firstName + person["lastName"]);
+
+const person2 = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function(){
+      return this.firstName + " " + this.lastName;
+    }
+};
+// . notation/statement
+
+let nameSurname = person2.fullName();
+console.log(nameSurname);
+
+
+
+const person3 = {
+  firstName: "Micheal", 
+  lastName:"Mic", 
+  age: 35
+};
+
+const myArray = Object.values(person3);
+
+console.log(myArray);
+
+// For döngüsü ile person3 objesi üzerinde
+// ilerleyerek degerlerini konsola yazdiriniz.
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
+
+myArray.forEach(p => console.log(p));
+
+var arr = ["a","b","c"];
+
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+  console.log(element);  
+}
+
+arr.forEach(function(element,index){
+    console.log(element,index);
+});
+
+
+let student = {
+  name: "John",
+  age: 20,
+  lessons: {
+    math: 70,
+    science: 20,
+  }
+};
+
+console.log(student.lessons.science);
+
+
+console.log("math" in student.lessons);
+
+for(let x in student.lessons){
+  //console.log(keys);
+  student.lessons[x] = student.lessons[x] + 5;
+  console.log(student.lessons[x]);
+}
+
+//student.lessons.math = 90;
+
+console.log(student);
