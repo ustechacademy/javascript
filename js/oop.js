686ccef27121de85fce4d9e7d310a5d8e438648d
ustechacademy <ustechacademytool@gmail.com>
2023-01-24 21:42:01 +0100

    Oop inheritance

--- js/oop.js
+++ js/oop.js
@@ -71,7 +71,7 @@ console.log(bmw.stop());
 
 // Getters ve Setters
 
-class Client{
+class Citizen{
     constructor(name,age,tc){
         this._name = name;
         this._age = age;
@@ -102,13 +102,102 @@ class Client{
 }
 
 let userType = "Lawyer";
-const person = new Client("John",45, 1256);
-person.tc = 1902;
+const person = new Citizen("John",45, 1256);
+//person.tc = 1902;
+Citizen.increaseClientCount();
 
 console.log(person.clientCount);
-console.log(Client.clientCount);
+console.log(Citizen.clientCount);
+
+const person2 = new Citizen("Emre",20, 11111);
+Citizen.increaseClientCount();
+console.log(person2)
+console.log(Citizen.clientCount);
 
 
+class Dog{
+    constructor(color,eyeColor,height,length,weight){
+        this._color = color;
+        this._eyeColor = eyeColor;
+        this._height = height;
+        this._length = length;
+        this._weight = weight;
+    }
+
+    sit(){
+        return "Sit down";
+    }
+
+    layDown(){
+        return "Lay down";
+    }
+
+    shake(){
+        return "Shake";
+    }
+
+    come(){
+        return "Come";
+    }
+}
 
 
+const sariKöpek = new Dog("Yellow","Brown",17,35,24);
 
+console.log(sariKöpek);
+
+console.log(sariKöpek.come());
+console.log(sariKöpek.layDown());
+console.log(sariKöpek.sit());
+console.log(sariKöpek.shake());
+
+//console.log(sariKöpek.come().layDown().sit().shake())
+
+// ES6 versiyonu JS 
+
+// Inheritance
+// Parent class
+class Animal{
+    constructor(name,species,color){
+        this._name = name;
+        this._species = species;
+        this._color = color;
+    }
+
+    makeSound(){
+        console.log(this._name + " makes sound");
+    }
+}
+
+// Child class
+class Bird extends Animal{
+
+    constructor(name,species,eyeColor){
+        super(name,species);
+        this._eyeColor = eyeColor;
+    }
+
+    fly(){
+        console.log(this._name + " flies.")
+    }
+
+    makeSound(){
+        console.log(this._name + " cik cik cik")
+    }
+
+}
+
+// Override 
+// Kus bir hayvandir 
+
+const bird = new Bird("Mavis","Parrot","Blue");
+
+console.log(bird);
+
+bird.fly();
+bird.makeSound();
+
+
+const animal = new Animal("Animal","Animal species","Yellow");
+
+console.log(animal);
