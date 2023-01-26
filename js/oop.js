05f8a155cbb001a039d1025989f05c5b767c8e1b
ustechacademy <ustechacademytool@gmail.com>
2023-01-26 21:47:11 +0100

    Oop encapsulation

--- js/oop.js
+++ js/oop.js
@@ -46,7 +46,7 @@ const job3 =  new Job(
     false
 )
 
-class Car{
+class Car1{
     constructor(color,brand,year,price,range){
         this.color = color; // sol taraf prop - sag taraf value
         this.brand = brand;
@@ -64,7 +64,7 @@ class Car{
     }
 }
 
-const bmw = new Car("Black","Bmw",1990,50000,150000);
+const bmw = new Car1("Black","Bmw",1990,50000,150000);
 
 console.log(bmw.start());
 console.log(bmw.stop());
@@ -172,8 +172,8 @@ class Animal{
 // Child class
 class Bird extends Animal{
 
-    constructor(name,species,eyeColor){
-        super(name,species);
+    constructor(name,species,color,eyeColor){
+        super(name,species,color);
         this._eyeColor = eyeColor;
     }
 
@@ -201,3 +201,103 @@ bird.makeSound();
 const animal = new Animal("Animal","Animal species","Yellow");
 
 console.log(animal);
+
+
+// Car -> Electrical Car -> Self Driving Car
+
+
+class Car{
+    constructor(range, model){
+        this.range = range;
+        this.model =model;
+    }
+
+    getModelRange(){
+        return this.range + " " + this.model;
+    }
+
+}
+
+class ElectricCar extends Car{
+
+    constructor(range,model,battery){
+        super(range,model);
+        this.battery = battery;
+    }
+
+    charge(){
+        return this.model + " is charging..";
+    }
+}
+
+class SelfDrivingCar extends ElectricCar{
+     constructor(range,model,battery,auto){
+        super(range,model,battery);
+        this.auto = auto;
+     }
+
+     drive(){
+        return this.model + "AI(Yapay zeka) is driving"
+     }
+}
+
+let mercedes = new SelfDrivingCar(0,"Mercedes",100,"Autopilot");
+
+console.log(mercedes.getModelRange());
+
+console.log(mercedes.charge());
+
+// Private, Protected, Public properties
+
+class Person{
+
+    #name;
+    #surname;
+    constructor(name,surname,age){
+        this.#name = name; // Private
+        this.#surname = surname; // Private
+        this._age = age; // Public  
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(newName){
+        this.#name = newName;
+    }
+
+    get surname(){
+        return this.#surname;
+    }
+
+    set surname(newSurname){
+        this.#surname = newSurname;
+    }
+    get age(){
+        return this._age;
+    }
+
+    set age(newAge){
+        if(newAge < 0){ // Protected logic
+            newAge = 0;
+        }
+        this._age = newAge;
+    }
+    
+    getName (){
+        return this.#name;
+    }
+}
+
+let p = new Person("John","GT",20);
+
+console.log(p.name);
+
+//p.name("Emre");
+p.name = "Emre";
+
+console.log(p.getName());
+console.log(p.name);
+p.age = -200;
+console.log(p.age);
