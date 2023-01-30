1be9ac0c543e8a18310742231146d66081b5656c
ustechacademy <ustechacademytool@gmail.com>
2023-01-30 20:12:12 +0100

    example update

--- js/inheritance-polymorphism.js
+++ js/inheritance-polymorphism.js
@@ -7,8 +7,11 @@
        Add a "type" property to "this" with a default
        value of "Shape"
     2. Add a method to "Shape" called "describe" that
-       will log out the following to the console:
+       will console log out the following to the console:
        "A ${this.colour} ${this.type}"
+
+       // Shape class -> - colour = "Transparent", type, describe
+
     3. Create a class called "Square" whose constructor
        takes 2 parameters (Square has to extend Shape):
        - colour
@@ -16,9 +19,14 @@
        Call the super constructor with "colour"
        Assign "sideLength" to the instance of "Square"
        Assign "Square" to the "type" property
+
+       // Square class -> colour, sideLength -> type = "Square"
+
     4. Add an "area" method to "Square" that will calculate
        and return the total area of the square.
        (Area of a square is: sideLength * sideLength)
+
+       // area method
     
     5. Create a class called "Rectangle" whose constructor
        takes 3 parameters (Rectangle has to extend Shape):
@@ -35,8 +43,8 @@
        technical detail as you can:
        const square = new Square("blue", 5);
        const rectangle = new Rectangle("red", 5, 6);
-       console.log( square.area() ); // 25
-       console.log( rectangle.area() ); // 30
+       console.log( square.area() ); // ?
+       console.log( rectangle.area() ); // ?
        for ( const shape of [square, rectangle] ) shape.describe();
        // A blue square
        // A red rectangle
